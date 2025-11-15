d2030628363cb165fee3dc283f9be119c72af4c4
Damián Bicocchi <pdamianbicocchi@gmail.com>
2025-11-14 23:03:20 -0300

    Terminado el ejercicio 5

--- ejercicio5/vacunas_base.pl
+++ ejercicio5/vacunas_base.pl
@@ -1,4 +1,39 @@
 % Hechos de vacunas %
+edad_minima(moderna, 0.5).
+edad_minima(spikevax, 0.5).
+edad_minima(comirnaty, 12).
+
+contraindicacion(moderna, anafilaxia).
+contraindicacion(moderna, reaccion_alergica_grave).
+contraindicacion(spikevax, anafilaxia).
+contraindicacion(spikevax, reaccion_alergica_grave).
+contraindicacion(comirnaty, anafilaxia).
+contraindicacion(comirnaty, reaccion_alergica_grave).
+
+% Personas %
+tiene_edad(claudia, 0.5).
+tiene_edad(hernan, 9).
+tiene_edad(clara, 12).
+tiene_edad(joaquin, 18).
+
+tiene_indicacion(claudia, nada).
+tiene_indicacion(clara, nada).
+tiene_indicacion(hernan, anafilaxia).
+tiene_indicacion(joaquin, reaccion_alergica_grave).
+
 
 % Reglas de vacunas %
+tiene_edad_recomendada(EdadMinima, Persona) :- 
+    tiene_edad(Persona, EdadPersona),
+    EdadPersona >= EdadMinima.
+
+inapto_indicacion(Persona, Vacuna) :- 
+    contraindicacion(Vacuna, Contraindicacion),
+    tiene_indicacion(Persona, Contraindicacion).
+
+puede_vacunarse(Persona, Vacuna) :- 
+    edad_minima(Vacuna, EdadMinima),
+    tiene_edad_recomendada(EdadMinima, Persona),
+    \+ inapto_indicacion(Persona, Vacuna).
+
 
